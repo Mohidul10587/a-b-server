2ec0b31baff1a08861c78c427febbfe444b4e6cf
mohid <mohid10587@gmail.com>
2025-10-30 11:10:42 +0600

    Send show in card

--- src/app/ordersOFSeller/sellerOrder.route.ts
+++ src/app/ordersOFSeller/sellerOrder.route.ts
@@ -8,13 +8,17 @@ import {
   //   updateOrderStatus,
   updateOrderStatusByAdmin,
 } from "./sellerOrder.controller";
-import { verifyAdminToken, verifySellerToken } from "../user/middlewares";
+import {
+  verifyAdminToken,
+  verifySellerAndAdminToken,
+  verifySellerToken,
+} from "../user/middlewares";
 import { deleteById } from "../shared/reusableControllers";
 import { SellerOrderModel } from "./sellerOrder.model";
 
 const router = express.Router();
 
-router.get("/getAllOrders", verifySellerToken, getAllOrders);
+router.get("/getAllOrders", verifySellerAndAdminToken, getAllOrders);
 router.get("/allOrdersForAdmin", verifyAdminToken, getAllOrdersForAdmin);
 
 router.post("/updateStatusByAdmin/:id", updateOrderStatusByAdmin);
